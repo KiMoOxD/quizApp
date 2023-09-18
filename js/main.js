ed1205f109b5be636faf89da6294a5e87208e967
KiMoOxD <25784985+KiMoOxD@users.noreply.github.com>
2023-09-19 00:12:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,79 +1,41 @@
-let c = 0, correct,wrng=0,crrct=0,curr=0;
+let c = 0, correct,wrng=0,crrct=0,curr=0,len=0;
 let response = [];
 
 
 
 $(function() {
-    $.ajaxSetup({ cache: false });
     $.ajax({
         type: "GET",
         url: "./js/Qs.json",
         dataType: "json",
-        contentType: 'multipart/form-data',
-        cache: false,
-	    headers: { "cache-control": "no-cache" },
         success: function (res) {
             console.log(res)
             response = res;
-            if (c >= res.length) {
-                console.log('Full')
-            } else {
-                loadQ(c);
-            }
-            
+            len = response.length;
+                //loadQ(c);
+                shuf(response)
+
         }
     });
 
-    function loadQ(index) {
-        document.querySelector('.question').textContent = response[index]['question']
-        correct = response[index]['answer']
-        document.getElementById('q1').textContent = response[index]['a']
-        document.getElementById('q2').textContent = response[index]['b']
-        document.getElementById('q3').textContent = response[index]['c']
-        document.getElementById('q4').textContent = response[index]['d']
-    }
 
     document.body.addEventListener('click', function(e) {
         if(e.target.classList.contains('ans')) {
             curr++;
-            document.querySelector('.prog').style.cssText = `width: ${curr/response.length * 100}%`
+            document.querySelector('.prog').style.cssText = `width: ${curr/len * 100}%`
 
             if (e.target.getAttribute('name') == correct) {
                 crrct++;
                 c++;
                 document.querySelector('.correct').textContent = crrct;
-                if (c >= response.length) { 
-                    setTimeout(() => {
-                        document.querySelector('.full').style.cssText = `
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;`
-                    }, 1000)
-                    
-                    setTimeout(() => {
-                        window.location.reload()
-                    }, 2000)
-                } else {
-                    loadQ(c);
-                }
+                    //loadQ(c);
+                    shuf(response)
             } else {
                 wrng++
                 c++;
                 document.querySelector('.wrong').textContent = wrng;
-                if (c >= response.length) { 
-                    setTimeout(() => {
-                        document.querySelector('.full').style.cssText = `
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;`
-                    }, 1000)
-                    
-                    setTimeout(() => {
-                        window.location.reload()
-                    }, 2000)
-                } else {
-                    loadQ(c);
-                }
+                    //loadQ(c);
+                    shuf(response)
             }
 
         }
@@ -82,3 +44,44 @@ $(function() {
     
 })
 
+
+function shuf(arr) {
+    if (arr.length >= 1) { 
+
+        let rand = Math.floor(Math.random() * arr.length)
+        let temp = arr[arr.length-1];
+        arr[arr.length-1] = arr[rand];
+        arr[rand] = temp;
+        
+        document.querySelector('.question').textContent = arr[arr.length-1]['question']
+        correct = arr[arr.length-1]['answer']
+        document.getElementById('q1').textContent = arr[arr.length-1]['a']
+        document.getElementById('q2').textContent = arr[arr.length-1]['b']
+        document.getElementById('q3').textContent = arr[arr.length-1]['c']
+        document.getElementById('q4').textContent = arr[arr.length-1]['d']
+
+        arr.length--;  
+    } else {
+        document.querySelector('.ans').style.cssText = `pointer-events: none;`
+        setTimeout(() => {
+            document.querySelector('.full').style.cssText = `
+        display: flex;
+        justify-content: center;
+        align-items: center;`
+        }, 1000)
+        
+        setTimeout(() => {
+            window.location.reload()
+        }, 2000)
+    }
+}
+
+
+    // function loadQ(index) {
+    //     document.querySelector('.question').textContent = response[index]['question']
+    //     correct = response[index]['answer']
+    //     document.getElementById('q1').textContent = response[index]['a']
+    //     document.getElementById('q2').textContent = response[index]['b']
+    //     document.getElementById('q3').textContent = response[index]['c']
+    //     document.getElementById('q4').textContent = response[index]['d']
+    // }
